e47af674017b043821045725eae8114315fd6ce8
Sara Maldum <saramaldum@hotmail.com>
2020-11-09 14:34:03 +0100

    Disabled the id input.

--- src/app/lan-detaljer/lan-detaljer.component.ts
+++ src/app/lan-detaljer/lan-detaljer.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import lan from '../../assets/lan.json';
-
 @Component({
   selector: 'app-lan-detaljer',
   templateUrl: './lan-detaljer.component.html',
@@ -13,8 +11,6 @@ export class LanDetaljerComponent implements OnInit {
   item;
 
   constructor(private route: ActivatedRoute) {
-    console.log(lan.lan);
-
     this.route.paramMap.subscribe((params) => {
       this.item = this.lanArray.find((item) => item.id === +params.get('id'));
     });
