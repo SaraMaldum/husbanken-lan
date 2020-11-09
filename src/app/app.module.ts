de834a986f6c08cd9a1ca941fc92c18b63470282
Sara Maldum <saramaldum@hotmail.com>
2020-11-09 15:57:36 +0100

    FormBuilder should send new info to the console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,13 @@ import { LanDetaljerComponent } from './lan-detaljer/lan-detaljer.component';
 
 @NgModule({
   declarations: [AppComponent, LanListeComponent, LanDetaljerComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/lan-detaljer/lan-detaljer.component.ts
+++ src/app/lan-detaljer/lan-detaljer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,10 +11,21 @@ export class LanDetaljerComponent implements OnInit {
   lanArray = JSON.parse(localStorage.getItem('data'));
   item;
 
-  constructor(private route: ActivatedRoute) {
+  loanDetailsForm = this.fb.group({
+    id: new FormControl(''),
+    lanetakere: new FormControl(''),
+    lanebelop: new FormControl(''),
+    notat: new FormControl(''),
+  });
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.route.paramMap.subscribe((params) => {
       this.item = this.lanArray.find((item) => item.id === +params.get('id'));
     });
+  }
+
+  onSubmit() {
+    console.log(this.loanDetailsForm.value);
   }
 
   ngOnInit() {}
